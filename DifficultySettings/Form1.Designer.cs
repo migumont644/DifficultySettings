4aec289e00050250b15148ad103535a9c1dcf69b
Trooper M <migmon23@hotmail.com>
2021-02-10 14:43:20 -0500

    Add project files.

---
+++ DifficultySettings/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace DifficultySettings
+{
+    partial class titleLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(titleLabel));
+            this.guardName = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.beginOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guardName
+            // 
+            this.guardName.AutoSize = true;
+            this.guardName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guardName.Location = new System.Drawing.Point(3, 42);
+            this.guardName.Name = "guardName";
+            this.guardName.Size = new System.Drawing.Size(231, 24);
+            this.guardName.TabIndex = 0;
+            this.guardName.Text = "Enter your name guard:";
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(240, 42);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(201, 29);
+            this.username.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Your Difficulty Levels";
+            // 
+            // easyButton
+            // 
+            this.easyButton.BackColor = System.Drawing.Color.Gray;
+            this.easyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.Location = new System.Drawing.Point(32, 183);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(121, 55);
+            this.easyButton.TabIndex = 3;
+            this.easyButton.Text = "Easy";
+            this.easyButton.UseVisualStyleBackColor = false;
+            this.easyButton.Click += new System.EventHandler(this.EasyButton_Click);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.BackColor = System.Drawing.Color.Gray;
+            this.mediumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.Location = new System.Drawing.Point(171, 183);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(121, 55);
+            this.mediumButton.TabIndex = 4;
+            this.mediumButton.Text = "Medium";
+            this.mediumButton.UseVisualStyleBackColor = false;
+            this.mediumButton.Click += new System.EventHandler(this.MediumButton_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.BackColor = System.Drawing.Color.Gray;
+            this.hardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.Location = new System.Drawing.Point(309, 183);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(121, 55);
+            this.hardButton.TabIndex = 5;
+            this.hardButton.Text = "Hard";
+            this.hardButton.UseVisualStyleBackColor = false;
+            this.hardButton.Click += new System.EventHandler(this.HardButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Gray;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(95, 258);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(263, 55);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // beginOutput
+            // 
+            this.beginOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginOutput.Location = new System.Drawing.Point(32, 357);
+            this.beginOutput.Name = "beginOutput";
+            this.beginOutput.Size = new System.Drawing.Size(398, 40);
+            this.beginOutput.TabIndex = 7;
+            this.beginOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(453, 406);
+            this.Controls.Add(this.beginOutput);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.guardName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "titleLabel";
+            this.Text = "Secuity Watch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label guardName;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label beginOutput;
+    }
+}
+
---
+++ DifficultySettings/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DifficultySettings
+{
+    public partial class titleLabel : Form
+    {
+        string heroName;
+        string difficultyLevel;
+
+        public titleLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void EasyButton_Click(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.Green;
+            mediumButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.Gray;
+
+            difficultyLevel = "easy";
+           
+        }
+
+        private void MediumButton_Click(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.Yellow;
+            hardButton.BackColor = Color.Gray;
+
+            difficultyLevel = "medium";
+
+        }
+
+        private void HardButton_Click(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.Red;
+
+            difficultyLevel = "hard";
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            heroName = username.Text;    
+            beginOutput.Text = $"Welcome {heroName} \n A true brave guard! Go ahead and take on {difficultyLevel} mode!";
+        }
+    }
+}
